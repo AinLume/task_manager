d7596fc1ba77c77ab459b47d1b33fd5c2947b609
AinLume <space.voidar@gmail.com>
2024-07-26 11:58:54 +0300

    Task manager

--- src/components/TaskManager.js
+++ src/components/TaskManager.js
@@ -4,6 +4,7 @@ import Task from './Task.js'
 function TaskManager () {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
+    const [tasks, setTasks] = useState([]);
 
     const handleChangeName = (event) => {
         setName(event.target.value);
@@ -13,21 +14,38 @@ function TaskManager () {
         setDescription(event.target.value);
     }
 
-    const handleSubmit = (name, description) => <Task name={name} description={description}/>;
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (name.trim() !== '' && description.trim() !== '') {
+            const new_task = {
+                id: tasks.length + 1,
+                name: name,
+                description: description
+            };
+            setTasks([...tasks, new_task]);
+            setName('');
+            setDescription('');
+        }
+    };
 
     return (
-        <form className='task-manage-form' onSubmit={handleSubmit}>
-            <label className='task-name-label'>
-                Название: 
-                <input type='text' className='task-name' value={name} onChange={handleChangeName}></input>
-            </label>
-            <label className='task-description-label'>
-                Описание:
-                <input type='text' className='task-description' value={description} 
-                onChange={handleChangeDescription}></input>
-            </label>
-            <button className='task-add'>Добавить</button>
-        </form>
+        <div className='container'>
+            <form className='task-manage-form' onSubmit={handleSubmit}>
+                <label className='task-name-label'>
+                    Название: 
+                    <input type='text' className='task-name' value={name} onChange={handleChangeName}></input>
+                </label>
+                <label className='task-description-label'>
+                    Описание:
+                    <input type='text' className='task-description' value={description} 
+                    onChange={handleChangeDescription}></input>
+                </label>
+                <button className='task-add' type='submit'>Добавить</button>
+            </form>
+            <div className='tasks-container'>
+                {tasks.map((task) => (<Task name={task.name} description={task.description} />))}
+            </div>
+        </div>
     );
 }
 
