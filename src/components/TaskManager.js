89c7185ec2d5b2bec96a0b493560a875708475c2
AinLume <space.voidar@gmail.com>
2024-07-30 13:27:46 +0300

    Task manager add columns

--- src/components/TaskManager.js
+++ src/components/TaskManager.js
@@ -5,6 +5,7 @@ import './taskmanager.css';
 function TaskManager () {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
+    const [columns, setColumn] = useState(0);
     const [tasks, setTasks] = useState([]);
 
     const handleChangeName = (event) => {
@@ -13,6 +14,10 @@ function TaskManager () {
 
     const handleChangeDescription = (event) => {
         setDescription(event.target.value);
+    }
+
+    const handleChangeColumn = (event) => {
+        setColumn(event.target.value);
     }
 
     const handleSubmit = (event) => {
@@ -36,10 +41,14 @@ function TaskManager () {
                     Название: 
                     <input type='text' className='task-name' value={name} onChange={handleChangeName}></input>
                 </label>
+                <label className='task-column-label'>
+                    Номер колонны: 
+                    <input type='text' className='task-column' value={columns} onChange={handleChangeColumn}></input>
+                </label>
                 <label className='task-description-label'>
                     Описание:
-                    <input type='text' className='task-description' value={description} 
-                    onChange={handleChangeDescription}></input>
+                    <textarea className='task-description' value={description} 
+                    onChange={handleChangeDescription}></textarea>
                 </label>
                 <button className='task-add' type='submit'>Добавить</button>
             </form>
